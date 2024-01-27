ea9698936c8ae03c2865750b467cf448dda0ef70
kbabis <krzysztofbabis@icloud.com>
2024-01-27 11:32:07 +0100

    #4 | Add AppearanceGroup as a convenience tool for previews

---
+++ Targets/Core/SwiftUpUI/Sources/SwiftUISupport/AppearanceGroup.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+public struct AppearanceGroup<Content: View>: View {
+    private let content: Content
+    
+    public init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+    
+    public var body: some View {
+        Group {
+            content
+                .preferredColorScheme(.light)
+                .previewDisplayName("Light Mode")
+            
+            content
+                .preferredColorScheme(.dark)
+                .previewDisplayName("Dark Mode")
+        }
+    }
+}
